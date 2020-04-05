b2e38c12c6e10eeecb3c7b29da5afb6bb04d3cd5
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-05 14:39:19 -0300

    Create main page

---
+++ huntweb/src/pages/main/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+import api from "../../services/api";
+
+export default class Main extends Component {
+  return() {
+    return <h1>Hello Rocket</h1>
+  }
+}
