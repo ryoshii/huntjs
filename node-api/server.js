1a09a1c7b1cdfc98168aaa2bbc8aa6c29339c6a5
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-05 12:17:45 -0300

    Install require-dir package

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
@@ -7,6 +8,8 @@ mongoose.connect("mongodb://localhost:27017/huntjs", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+requireDir("./src/models");
 
 app.get("/", (request, response) => {
   response.send("Hello");
